523ad824f9de35566da678c8602ef64efcf09b91
3jun <kk3june@gmail.com>
2022-04-23 19:37:41 +0900

    :lipstick: 카테고리 페이지 레이아웃 blueprint 구현

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  width: 80%;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: salmon;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  width: 100%;
+  justify-content: center;
+  background-color: white;
+  gap: 5px;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+`;
+
+const GridItem = styled.div`
+  background-color: black;
+  height: 30rem;
+  &:hover {
+    background-color: white;
+  }
+`;
 
 function Category() {
-  return <div>Category</div>;
+  return (
+    <div>
+      <Section>
+        Category
+        <GridContainer>
+          <GridItem>dd</GridItem>
+          <GridItem>dd</GridItem>
+          <GridItem>dd</GridItem>
+          <GridItem>dd</GridItem>
+          <GridItem>dd</GridItem>
+          <GridItem>dd</GridItem>
+        </GridContainer>
+      </Section>
+    </div>
+  );
 }
 
 export default Category;
