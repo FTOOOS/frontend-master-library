59fdf09d95f4988e9b1f9a9a2e985b3451b861c7
Lela12 <sharryun@gmail.com>
2022-04-24 04:20:40 +0900

    :lipstick: 3d book 추가

---
+++ src/components/Book.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import book1 from "../assets/bookcover/book.png";
+
+const Book = () => {
+  return (
+    <ImageContainer>
+      <BookImageWrap>
+        <BookImage>
+          <img src={book1} alt="book" />
+        </BookImage>
+      </BookImageWrap>
+    </ImageContainer>
+  );
+};
+
+export default Book;
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: 60px;
+`;
+
+const BookImageWrap = styled.div`
+  &:before {
+    display: block;
+    content: "";
+    height: 20%;
+    width: 45%;
+    position: absolute;
+    background-image: url(https://i.imgur.com/5udcDlp.png);
+    background-size: 90% auto;
+    background-repeat: no-repeat;
+    top: 84%;
+    left: 35%;
+    right: 0;
+    transition: all 0.2s ease-out;
+    transform-origin: 30% 50%;
+    pointer-events: none;
+  }
+
+  &:hover {
+    position: relative;
+  }
+`;
+
+const BookImage = styled.div`
+  width: 100%;
+  max-width: 20px;
+  will-change: transform;
+  transform-origin: 0 100%;
+  transition: transform 0.3s ease-out;
+
+  &:hover {
+    transform: rotate(-10deg);
+  }
+
+  .img {
+    width: 100% !important;
+  }
+`;
