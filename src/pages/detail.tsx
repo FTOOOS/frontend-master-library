53882ba3a01f366194e5f98d84bcfb369847cbeb
choisohyun <sohyun4283@naver.com>
2022-04-24 14:46:09 +0900

    :zap: 책 상세 페이지 레이아웃 수정

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { shareKakao } from "../api/shareKakao";
+import kakaoIcon from "../assets/ico_sns_kakao.svg";
 
 const Section = styled.div`
   width: 80%;
   height: 100vh;
   margin: 100px auto 0;
-  background-color: #b2bec3;
 `;
 
 const FlexWrapper = styled.div`
@@ -15,49 +15,117 @@ const FlexWrapper = styled.div`
   justify-content: space-between;
 `;
 
+const CoverWrapper = styled.div`
+  width: 100%;
+  img { 
+    position: absolute;
+    top: 0; 
+    left: 0; 
+    max-width: 100%; 
+    height: auto; 
+    transform: translate(-50%,-50%); 
+  }
+`
+
+const Thumbnail  = styled.div`
+  position: relative; 
+  padding-top: 100%; 
+  overflow: hidden;
+`
+
+const Center = styled.div`
+  position: absolute; 
+  top: 0; 
+  left: 0; 
+  right: 0; 
+  bottom: 0;
+  transform: translate(50%,50%);
+`
+
 const Cover = styled.img`
   width: 20rem;
-  height: 30rem;
-  margin-right: 30px;
 `;
 
 const BookInfo = styled.div`
   width: 100%;
+  color: #f0efef;
+  padding: 30px;
 `;
 
-const Title = styled.div`
+const Title = styled.h1`
   font-weight: 800;
   font-size: 2rem;
   line-height: 2.2rem;
 `;
-const SubTitle = styled.div`
+
+const SubTitle = styled.h2`
   font-size: 1.2rem;
   line-height: 1.5rem;
-  margin-bottom: 1.5rem;
+  margin: 1rem 0;
 `;
 
-const Author = styled.span`
-  margin-right: 2rem;
+const HorizontalLine = styled.hr`
+  border-top: 0;
+  border-bottom: solid 1px #d8d8d8;
 `;
+
+const VerticalLine = styled.em`
+  margin: 4px 4px 0 4px;
+  width: 1px;
+  height: 12px;
+  text-indent: -999em;
+  border-right: solid 1px #ccc;
+  overflow: hidden;
+  text-align: left;
+  vertical-align: top;
+  display: inline-block;
+  zoom: 1;
+`
+
+const Author = styled.span``;
+
 const Publisher = styled.span``;
+
 const Summary = styled.p`
   width: 100%;
   margin: 1rem 0;
   white-space: pre-wrap;
+  color: #f0efef;
 `;
+
 const PurchaseLink = styled.a`
   display: block;
   width: 100px;
-  height: 30px;
-  border-radius: 5px;
-  background-color: white;
+  height: 50px;
   line-height: 30px;
   text-align: center;
   text-decoration: none;
   color: black;
+  background: #ECECEC;
+  border-radius: 10px;
+  cursor: pointer;
+  
+ & + & {
+   margin-left: 20px;
+ }
 `;
 
-const SharingBtn = styled.button``;
+const SharingBtn = styled.a`
+  cursor: pointer;
+  width: 56px;
+  position: relative;
+  display: block;
+  
+  &:after {
+    background: url(${kakaoIcon}) no-repeat 0 0;
+    position: absolute;
+    width: 56px;
+    height: 56px;
+    transform: translateX(-50%);
+    content: "";
+    left: 50%;  
+  }
+`;
 
 interface DetailProps {
   id: string;
@@ -82,7 +150,7 @@ function Detail({ match }: any) {
     bookSubTitle: "",
     author: "",
     publisher: "",
-    img: "",
+    img: "https://i.imgur.com/HxbUvqG.png",
     kyobo: "",
     sharing: "",
     summary: "",
@@ -91,7 +159,7 @@ function Detail({ match }: any) {
 
   useEffect(() => {
     setParam(+id - 1);
-    fetch("http://localhost:3000/data/bookMock.json", {})
+    fetch(window.location.origin + "/data/bookMock.json", {})
       .then((response) => response.json())
       .then((data) => {
         const entire = data.data;
@@ -109,44 +177,68 @@ function Detail({ match }: any) {
     <div>
       <Section>
         <FlexWrapper>
-          <Cover src={selectedBook.img} />
+          <CoverWrapper>
+            <Thumbnail>
+              <Center>
+                <Cover className="centered" src={selectedBook.img} />
+              </Center>
+            </Thumbnail>
+          </CoverWrapper>
           <BookInfo>
             <Title>{selectedBook.bookTitle}</Title>
             <SubTitle>{selectedBook.bookSubTitle}</SubTitle>
-            <div>
               <Author>{selectedBook.author}</Author>
+              <VerticalLine>|</VerticalLine>
               <Publisher>{selectedBook.publisher}</Publisher>
-            </div>
-            <Summary>{selectedBook.summary}</Summary>
-            <FlexWrapper>
-              <PurchaseLink
-                href={selectedBook.kyobo}
-                target="_blank"
-                rel="noreferrer"
-              >
-                교보문고
-              </PurchaseLink>
-              <PurchaseLink
-                href={selectedBook.yes24}
-                target="_blank"
-                rel="noreferrer"
-              >
-                yse24
-              </PurchaseLink>
-            </FlexWrapper>
-            <SharingBtn
-              onClick={() => {
-                shareKakao(
-                  selectedBook.bookTitle,
-                  selectedBook.img,
-                  "http://localhost:3000/detail/javascript/1"
-                );
-              }}
-            >
-              카카오톡 공유하기
-            </SharingBtn>
+            <HorizontalLine/>
+            <table>
+              <colgroup>
+                <col width="110"/>
+                <col width="*"/>
+              </colgroup>
+              <tbody>
+                <tr>
+                  <th scope="row">구매링크 바로가기</th>
+                  <td>
+                    <FlexWrapper>
+                      <PurchaseLink
+                        href={selectedBook.kyobo}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        교보문고
+                      </PurchaseLink>
+                      <PurchaseLink
+                        href={selectedBook.yes24}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        yse24
+                      </PurchaseLink>
+                    </FlexWrapper>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">링크 공유하기</th>
+                  <td>
+                    <SharingBtn
+                      onClick={(e) => {
+                        e.preventDefault();
+                        shareKakao(
+                          selectedBook.bookTitle,
+                          selectedBook.img,
+                          window.location.href
+                        );
+                      }}
+                    >
+                    </SharingBtn>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </BookInfo>
         </FlexWrapper>
+        <Summary>{selectedBook.summary}</Summary>
       </Section>
     </div>
   );
