642de1b67426bedb8aff8cab724e41e1898d0534
choisohyun <sohyun4283@naver.com>
2022-04-24 12:26:56 +0900

    :sparkles: 메인화면 정리

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import "reset-css";
 import { GlobalStyles } from "./styles";
 import initialize from "./api/initialize";
+import logo from "./assets/logo.png";
 
 const Header = styled.header`
   position: fixed;
@@ -31,13 +32,13 @@ function App() {
     <BrowserRouter>
       <Header>
         <Link to={"/"} style={{ textDecoration: "none", color: "white" }}>
-          프론트엔드 장인의 서재
+          <img src={logo} alt="FTOOOS" style={{ height: "30px" }} />
         </Link>
       </Header>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<RoadMap />}></Route>
-        <Route path="/category/:group" element={<Category />}></Route>
+        <Route path="/" element={<RoadMap/>}/>
+        <Route path="/category/:group" element={<Category/>}/>
         <Route path="/detail/:group/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
