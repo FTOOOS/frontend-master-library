b3d3cb4b08be4c6316b89dac5afa509ee6dce343
3jun <kk3june@gmail.com>
2022-04-24 18:48:20 +0900

    :lipstick: footer 컴포넌트 position bottom fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,27 +3,27 @@ import styled from "styled-components";
 import github from "../assets/github.svg";
 
 const FooterContainer = styled.div`
-  position: relative;
-  margin-top: 150px;
-  height: 60px;
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  line-height: 50px;
+  width: 100%;
+  bottom: 0;
+  height: 50px;
   background: #666;
 `;
 
-const FooterText = styled.div`
-  position: absolute;
-  bottom: 22px;
-  right: 85px;
+const FooterText = styled.span`
+  text-align: right;
   color: white;
   font-size: 12px;
 `;
 
-const GitHubWrapper = styled.span`
-  position: absolute;
-  bottom: 12px;
-  right: 25px;
+const GitHubWrapper = styled.div`
   width: 50px;
   height: 40px;
-  display: block;
+  display: inline-block;
   background: url(${github}) no-repeat center center / 100% 100%;
   cursor: pointer;
 `;
@@ -34,10 +34,12 @@ function Footer() {
       <FooterText>
         &copy; {new Date().getFullYear()} 테오의 스프린트 7기
       </FooterText>
+
       <a
         target="_blank"
         href="https://github.com/FTOOOS/frontend-master-library"
         rel="noreferrer"
+        style={{ height: "100%", display: "flex", alignItems: "center" }}
       >
         <GitHubWrapper />
       </a>
