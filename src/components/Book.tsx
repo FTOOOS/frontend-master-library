2006ae6d78e4b98c033b12158f5c1ba947487838
Lela12 <sharryun@gmail.com>
2022-04-25 15:32:34 +0900

    :wrench: bookcover 이미지 추가

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import book1 from "../assets/bookcover/book-removebg.png";
+import book1 from "../assets/bookcover/1-1.png";
 
 const Book = () => {
   return (
